074c5a5368ba50db95fc2a042af77e9da02f78b0
felix <funal.zhang@gmail.com>
2014-04-27 21:35:38 +0800

    Try to use company mode to replace auto-complete.
        This is because prelude support company mode.

--- init-robe.el
+++ init-robe.el
@@ -4,7 +4,8 @@
 (prelude-require-package 'robe)
 
 (add-hook 'ruby-mode-hook 'robe-mode)
-(add-hook 'robe-mode-hook 'ac-robe-setup)
+;; (add-hook 'robe-mode-hook 'ac-robe-setup)
+(push 'company-robe company-backends)
 
 (provide 'init-robe)
 ;;; init-robe.el ends here
