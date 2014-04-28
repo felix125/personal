5d30d5a7abaf4a7336a15c5c98d0d84b85e65d9d
felix <funal.zhang@gmail.com>
2014-04-29 02:28:08 +0800

    add some key-chord

---
+++ init-key-chord.el
@@ -0,0 +1,18 @@
+;;; init-key-chord.el --- Define key chord key binding
+;;; Commentary:
+;;; Code:
+
+(key-chord-define-global "==" 'er/expand-region)
+(key-chord-define-global "ff" 'ido-find-file)
+(key-chord-define-global "dd" 'kill-word)
+(key-chord-define-global "bd" 'backward-kill-word)
+(key-chord-define-global "pp" 'move-text-up)
+(key-chord-define-global "nn" 'move-text-down)
+(key-chord-define-global "pf" 'projectile-find-file)
+(key-chord-define-global "ww" 'helm-swoop)
+
+
+
+
+(provide 'init-key-chord)
+;;; init-key-chord.el ends here
