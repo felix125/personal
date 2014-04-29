20fb326b7394f75f25136a7efd1145f559b4b01e
felix <funal.zhang@gmail.com>
2014-04-29 09:40:50 +0800

    change the key binding

--- init-key-chord.el
+++ init-key-chord.el
@@ -2,9 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
-(key-chord-define-global "==" 'er/expand-region)
+(key-chord-define-global "c=" 'er/expand-region)
 (key-chord-define-global "ff" 'ido-find-file)
-(key-chord-define-global "dd" 'kill-word)
+(key-chord-define-global "dd" 'kill-whole-line)
 (key-chord-define-global "bd" 'backward-kill-word)
 (key-chord-define-global "pp" 'move-text-up)
 (key-chord-define-global "nn" 'move-text-down)
